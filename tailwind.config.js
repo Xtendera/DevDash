ce9e2e4f9e712eb26f78ab95824cc09d2ae99895
xtendera <geektraindev@gmail.com>
2025-10-26 14:05:23 -0500

    Added shadcn, vite, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+});
